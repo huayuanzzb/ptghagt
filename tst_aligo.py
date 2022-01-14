4abdd95e6a22b6c858e21e935c92b6b4dff05de6
icedance <961590104@qq.com>
2022-01-14 09:13:45 +0800

    Update tst_aligo.py

--- tst_aligo.py
+++ tst_aligo.py
@@ -3,7 +3,7 @@ from aligo import Aligo
 if __name__ == '__main__':
   ali = Aligo(refresh_token='123456')
   user = ali.get_user()  # 获取用户信息
-    ll = ali.get_file_list()  # 获取网盘根目录文件列表
-    # 遍历文件列表
-    for file in ll:
-        print(file.file_id, file.name, file.type)
+  ll = ali.get_file_list()  # 获取网盘根目录文件列表
+  # 遍历文件列表
+  for file in ll:
+    print(file.file_id, file.name, file.type)
